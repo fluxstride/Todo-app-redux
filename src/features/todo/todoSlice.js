ae9cc09cc1d78f085fb53c58c87d1ded5fc7a3a4
samuel adekoya <samueladexx@gmail.com>
2022-03-20 19:57:09 +0100

    -

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -1,20 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = [];
+const savedState = localStorage.getItem('state')
+const initialState = JSON.parse(savedState);
 const todoSlice = createSlice({
   name: "todo",
   initialState,
   reducers: {
     addTodo: (state, action) => {
       state.push(action.payload);
+      localStorage.setItem('state', JSON.stringify(state))
     },
     removeTodo: (state, action) => {
       const index = state.findIndex(todo => todo.id === action.payload.id)
       state.splice(index, 1)
+      localStorage.setItem('state', JSON.stringify(state))
     },
     editTodo: (state, action) => {
       const index = state.findIndex(todo => todo.id === action.payload.id)
       state[index].text && (state[index].text = action.payload.text)
+      localStorage.setItem('state', JSON.stringify(state))
     }
   }
 });
