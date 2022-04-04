a884f609a18598911e9b30007d12a4c63ee5270f
samuel adekoya <samueladexx@gmail.com>
2022-04-04 15:52:31 +0100

    init

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <Provider store={store}>
+  <>
     <TodoApp />
-  </Provider>,
+  </>,
 
 );
 
